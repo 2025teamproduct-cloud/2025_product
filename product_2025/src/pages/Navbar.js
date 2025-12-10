1859e8fd561667953f1e1fcd7acc67b25ecf2949
2025teamproduct-cloud <2025teamproduct@gmail.com>
2025-12-10 05:25:41 +0000

    update

--- product_2025/src/pages/Navbar.js
+++ product_2025/src/pages/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
-import SimpleLogo from "./img/Simple.jpg";
+import SimpleLogo from "./img/Simple.png";
 
 const Navbar = () => {
   return (
