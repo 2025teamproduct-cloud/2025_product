4b94aa4f5962f331a3427b69de05473e08c45c99
2025teamproduct-cloud <2025teamproduct@gmail.com>
2025-12-10 04:35:03 +0000

    update

--- product_2025/src/pages/Navbar.js
+++ product_2025/src/pages/Navbar.js
@@ -1,18 +1,27 @@
-// src/components/Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import imageFile from '../img/Image.jpg';
 
 const Navbar = () => {
   return (
     <header className="navbar">
       <div className="navbar-inner">
+
         <h1 className="navbar-title">
-          <Link to="/" className="navbar-home-link">近場のグルメ season2</Link>
+          <Link to="/" className="navbar-home-link">
+            <img 
+              src={imageFile} 
+              alt="近場のグルメ season2 ロゴ"
+              className="navbar-logo"
+            />
+          </Link>
         </h1>
+
         <div className="navbar-link">
           <Link to="/mypage">マイページへ</Link>
         </div>
+
       </div>
     </header>
   );
